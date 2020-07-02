381dd9dcd9a76db066fb62af9bbe702f32be4417
Clovis Mugaruka <clov@iris.local>
2020-07-02 14:48:11 +0300

    cellular simulation initial code

---
+++ README.md
@@ -0,0 +1,54 @@
+# A very basic Game of life simulation
+
+If you don't know about Conway's Game of Life, 
+start by reading [this article](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+
+### Good articles / videos
+1. Daniel Shiffman's [Coding Challenge #85](https://www.youtube.com/watch?v=FWSR_7kZuYg)
+2. [How to write Conway's "Game of Life" in Python](https://medium.com/better-programming/how-to-write-conwells-game-of-life-in-python-c6eca19c4676)
+3. [What are cellular Automata](https://www.youtube.com/watch?v=E7CxMHsYzSs) by [javidx9](https://community.onelonecoder.com/)
+
+
+##### Requirements
+
+```console
+
+You are tasked with creating a program capable of generating a random starting cellular matrix based
+upon a given seed value. The program must then solve and print out the next 100 time-steps of a
+modified game of life simulation. The simulation is guided by a handful of simplistic rules that will result
+in a seemingly complex simulation of cellular organisms.
+
+Below is an example starting cellular matrix when given an input of 10 rows and 20 columns:
+
+	00.0.00....0000..000
+	000..0.0.0.0000.0.0.
+	00000...0000...0.0.0
+	..0.00.0....0000..00
+	.0.0..00....0....0..
+	..00.00.00..0000....
+	0....00..0.0...00..0
+	.00..00.0..0...0.0.0
+	.00.0.00..00....0000
+	0.00..0......00.000.
+
+Develop a program capable of generating a random starting cellular matrix of a size directed by the user.
+Your generator must abide by the following rules:
+1) The starting matrix must only contain the following symbols:
+a. Periods ‘.’ to signify currently “dead” cells.
+b. Capital O’s to signify currently “alive” cells.
+2) The starting matrix must be randomly generated making use of the seed value provided by the
+user. As such:
+a. Matrices generated using identical seeds should be identical.
+b. Matrices generated using difference seeds should be different.
+
+Using this starting cellular matrix, your program should then simulate the next 100 steps of a simulation
+that uses the following rules to dictate what occurs during each time step:
+1) Any position in the matrix with a period ‘.’ is considered “dead” during the current time step.
+2) Any position in the matrix with a capital ‘O’ is considered “alive” during the current time step.
+3) If an “alive” square has exactly two, three or four living neighbors, it continues to be “alive” in
+the next time step.
+4) If a “dead” square has an even number greater than 0 living neighbors, it will be “alive” in the
+next time step.
+5) Every other square dies or remains dead, causing it to be “dead” in the next time step.
+```
+
---
+++ app.py
@@ -0,0 +1,85 @@
+import random
+
+# symbols representing dead or alive cells
+DEAD = '.'
+ALIVE = '0'
+
+def init(rows, cols, seed):
+    # to initialize the random number generator
+    if seed: # if '' use python defined seed ~> see: https://docs.python.org/3/library/random.html
+        random.seed(seed)
+    # populate matrix with 0's
+    matrix = [ [j * 0 for j in range(cols)] for i in range(rows) ]
+    # populate matrix of i rows and j columns with 0's and *'s
+    for i in range(rows):
+        for j in range(cols):
+            matrix[i][j] = ALIVE if random.randint(0, 1) else DEAD
+    return matrix
+
+def build(matrix, rows, cols, seed, _100th_step=100):
+    print('\nStarting Cellular Matrix ⚡☄️💨🚀')
+    draw(matrix, cols)
+    while _100th_step > 0:
+        next_matrix = init(rows, cols, seed)
+        for i in range(rows):
+            for j in range(cols):
+                state = matrix[i][j] # get current cell value
+                # get total number of living and dead neighbor of cell
+                n_alive, _ = get_neighbors(matrix, i, j, rows, cols)
+                if state == ALIVE and n_alive in (2,3,4) \
+                        or state == DEAD and ((n_alive % 2 == 0) and n_alive > 0):
+                    next_matrix[i][j] = ALIVE
+                else:
+                    next_matrix[i][j] = DEAD
+        matrix = next_matrix
+        _100th_step -= 1
+    print('\nFinal Cellular Matrix⚡☄️💨🚀')
+    draw(matrix, cols)
+
+def get_neighbors(matrix, x_index, y_index, total_number_of_rows, total_number_of_cols):
+    result = {'dead': 0, 'alive': 0}
+    for row in range(-1, 2):
+        for col in range(-1, 2):
+            neighbor_row = x_index + row
+            neighbor_col = y_index + col
+            # check to see if it's a valid neighbor
+            if neighbor_row == x_index and neighbor_col == y_index:
+                continue # cell shouldn't count itself as neighbor
+            elif neighbor_row < 0 or neighbor_row >= total_number_of_rows:
+                continue
+            elif neighbor_col < 0 or neighbor_col >= total_number_of_cols:
+                continue
+            else:
+                state = matrix[neighbor_row][neighbor_col]
+                if state == DEAD:
+                    result['dead'] += 1
+                else:
+                    result['alive'] += 1
+    return result['alive'], result['dead']
+
+def draw(matrix, width):
+    """beautify grid on terminal."""
+    # TODO: instead of printing to the terminal, save to file
+    header = "==** Game of life **=="
+    footer = "==** .. **=="
+    print(f"\n\t{header:^{width}}")
+    for el in matrix:
+        print(f"\t{''.join(el)}")
+    print(f'\t{footer:^{width}}')
+
+if __name__ == '__main__':
+    print("let's generate a cellular matrix 🎲 📌\n")
+    q = 'yes'
+    while q.lower() not in ('n', 'no') :
+        try:
+            rows = int(input('Number of rows: '))
+            columns = int(input('Number of columns: '))
+            seed = input("Seed (or leave empty if you want to use the system generarted seed): ")
+            # TODO: validate rows and columns
+            matrix = init(rows, columns, seed)
+            build(matrix, rows, columns, seed)
+        except ValueError as e: # TODO: catch any other error/exception that may be thrown
+            print(f'An error occurred - {e}\nPlease try again..')
+        finally:
+            q = input('Would you like to generate another cellular matrix [Y/N]?: ')
+    print("🎆🎉🎇 Thank you using the program 🎆🎉🎇")
